837c9e5b695ca8dbfdb4067e89cff75ae0e855f8
James Bedford <jamesbedford@protonmail.com>
2021-07-24 22:13:18 +1000

    Added some comments

--- Program.cs
+++ Program.cs
@@ -13,25 +13,20 @@ namespace DegreeArray
 
             var graph = graphImporter.ImportGraph("C:/Users/masterofdoom/code projects/C#/DegreeArray/rosalind_deg.txt").ToList();
 
-            foreach (string[] edge in graph)
-            {
-                Console.WriteLine($"{edge[0]} {edge[1]}");
-            }
-
             int numberOfVertices = Int32.Parse(graph[0][0]);
 
             List<int> degreeOfVertex = new() { 0 };
 
             using StreamWriter sw = new("C:/Users/masterofdoom/code projects/C#/DegreeArray/solution.txt");
             {
-                for (int i = 1; i <= numberOfVertices; i++)
+                for (int i = 1; i <= numberOfVertices; i++) // Starting at 1 to ignore the first position in the list which corresponds to m and n and not to an edge.
                 {
                     degreeOfVertex.Add(0);
                     for (int j = 1; j < graph.Count; j++)
                     {
                         string iString = i.ToString();
                         var edge = graph[j];
-                        if (edge[0] == iString || edge[1] == iString)
+                        if (edge[0] == iString || edge[1] == iString) // Every edge a vertex is part of increases its degree of vertex by 1.
                         {
                             degreeOfVertex[i] += 1;
                         }
